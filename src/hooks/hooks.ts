8c0d5809eac9bbccf6e3fa3477b6a53ed029182e
Vinayakkkk <vinayakhanagi1234@gmail.com>
2025-12-19 16:48:53 +0530

    my

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,7 +1,8 @@
 import { Before, After } from '@cucumber/cucumber';
 import { CustomWorld } from '../support/world';
+import { setDefaultTimeout } from '@cucumber/cucumber';
 
-
+setDefaultTimeout(60 * 1000);
 
 Before(async function (this: CustomWorld) {
   await this.init();
